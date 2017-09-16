339b4943c61040a77970535f803203c74c0dd4ff
gusdlf93 <gusdlf93@naver.com>
2017-09-17 01:12:01 +0900

    FIrst Fetch

---
+++ src/S_Team.java
@@ -0,0 +1,25 @@
+
+public class S_Team {
+
+	S_Team()
+	{
+		System.out.println("Hello World!");
+	}
+	
+	private void TeamLeader()
+	{
+		System.out.println("Leader : 32121875 ±èÇöÀÏ");
+	}
+	//private void Engineer() 
+	//private void Reverser()
+	//private void Supporter()
+	
+	public static void main(String[] args) {
+		S_Team Team = new S_Team();
+		Team.TeamLeader();
+		//TEam.Engineer();
+		//Team.Reverser();
+		//Team.Supporter();		
+	}	
+	
+}
